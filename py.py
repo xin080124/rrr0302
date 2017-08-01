92edcc3b66ec61de7718a785a33f65fc39887bde
nancyUnitec <xin080124@hotmail.com>
2017-08-02 10:07:05 +1200

    add

--- py.py
+++ py.py
@@ -1,3 +1,58 @@
+
+
+#print in python
+strHello = "the length of (%s) is %d" %('Hello World',len('Hello World'))
+print strHello
+
+####################create json in python#############################################
+file_object = open("moviestest.txt","r")
+
+path_out = 'result.txt'
+
+file_out = open(path_out,'w')
+lines = file_object.readlines()
+lineContent = ''
+i=0
+for line in lines:
+    line = line.replace('\n','')
+    i = i+1
+    print(('\n i = %d ')%(i))
+    tag = 'hehe'
+    if i == 2:
+        tag = 'id'
+    elif  i==3:
+        tag ='title'
+    elif  i==4:
+        tag ='category'
+    elif  i==5:
+        tag ='desc'
+    elif  i==6:
+        tag ='popularity'
+    elif  i==7:
+        tag ='onsale'
+    elif  i==8:
+        tag ='price'
+    else:
+        tag ='end'
+
+    if i != 1 and i != 9:
+        jsonLine = "\""+tag+"\":\"" + line + "\","
+        lineContent += jsonLine
+    # lineContent = lineContent.replace('\n',' ')
+        print(lineContent)
+    if i==9:
+        file_out.write("'{ "+lineContent+"},'+\n") 
+        lineContent = ''
+        i=0
+# file_out.write("hehe "+lineContent)
+print ("\nsuccess")
+#this is a stream operation, or the next file_object
+#will get nothing
+file_object.close()
+file_out.close()
+
+####################################################################################
+
 from cassandra.cluster import Cluster
 cluster = Cluster(['127.0.0.1'])
 session = cluster.connect()
