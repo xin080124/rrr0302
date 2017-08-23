951e557ff562104c5842afac9a6bc1295b9af3b8
nancyUnitec <xin080124@hotmail.com>
2017-08-24 10:27:36 +1200

    add code for reviewing C code

---
+++ forC/forC/forC.cpp
@@ -0,0 +1,20 @@
+// forC.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<string.h> 
+
+int main()
+{
+	printf("hello, vc++");
+
+	int x = 0; 
+	int y = 0;
+	//char str[10];
+	char str[1];
+	x = strlen(str);
+	y = sizeof(str);
+
+    return 0;
+}
+
