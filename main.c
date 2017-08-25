104b82408500b1f30ac673e9c0fd16f7bc171677
nancyUnitec <xin080124@hotmail.com>
2017-08-25 21:38:57 +1200

    add code to practice gdb

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include<stdio.h>
+#include<string.h> 
+
 
 int main(void)
 {
     //printf("hello, ubuntu again~!");
+	
+	//char str[10];
+
+	int x = 0; 
+    int y = 0;
+	
+	char str[1];
+
+	x = strlen(str);
+    y = sizeof(str);
+	
     int res = sum(10,15);
 	printf(" the return value is %d",res);
 	
