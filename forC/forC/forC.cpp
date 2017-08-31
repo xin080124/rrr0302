76623edc4108911cae9c8a73f87804c9d43dddea
nancyUnitec <xin080124@hotmail.com>
2017-08-31 21:44:06 +1200

    add file for c++

--- forC/forC/forC.cpp
+++ forC/forC/forC.cpp
@@ -4,7 +4,18 @@
 #include "stdafx.h"
 #include<string.h> 
 
+#include<stdio.h> 
+#include<conio.h> 
+
+#include "forCPP.h"
+
 void test(char **p);
+void showTheSizeOfBlock();
+void switchChars(char *str, int step);
+void memcpyChars(char *str, int step);
+
+void *getInstance(int userInput);
+
 
 int main()
 {
@@ -12,8 +23,8 @@ int main()
 
 	int x = 0; 
 	int y = 0;
-	//char str[10];
-	char str[1];
+	char str[10];
+	//char str[1];
 	x = strlen(str);
 	y = sizeof(str);
 
@@ -25,8 +36,38 @@ int main()
 	char *pchar = NULL;
 	char **pp = &pchar;
 
-	test(pp);
+    test(pp);
 
+	char *p12 = NULL;
+	int s12 = sizeof(p12);
+
+	char *p121 = new char[100];
+	int s121 = sizeof(p121);
+
+	//showTheSizeOfBlock();
+
+	char *swc = "abcde";
+	switchChars(swc, 2);
+
+	char *mcc = "abcdefghi";
+	memcpyChars(mcc, 2);
+
+	//withCC base;
+	//base.showCallOrder();
+
+	subCC sub;
+	sub.showCallOrder();
+
+
+	void *testVirtual = NULL;
+	int input;
+	scanf("%d", &input);
+	
+	printf("the input is %d", input);
+	
+	//testVirtual = getInstance(input);
+	//testVirtual->showCallOrder();
+	
 	return 0;
 }
 
@@ -35,3 +76,63 @@ void test(char **p)
 	*p = new char[100];
 }
 
+void showTheSizeOfBlock()
+{
+	unsigned char *p1;
+	unsigned long *p2;
+
+	p1 = (unsigned char *)0x801000;
+	p2 = (unsigned long *)0x810000;
+
+	unsigned char *pa1 = p1 + 5;
+	unsigned long *pa2 = p2 + 1;
+
+	printf("pa2 = %d", (int)pa2);
+}
+
+#define MAX_LEN 100
+
+void switchChars(char *str, int step)
+{
+	int len = strlen(str);
+	char dest[MAX_LEN];
+	memset(dest, 0, len+1);
+	printf("len = %d", len);
+
+	strcpy(dest, str + len - step);
+	strcpy(dest + step, str);
+	
+	memset(dest+len, 0, 1);
+
+
+	printf("res = %s", dest);
+}
+
+void *getInstance(int userInput)
+{
+	void *p;
+	if (userInput == 0)
+		p = new withCC;
+	else
+		p = new subCC;
+
+	return p;
+}
+
+void memcpyChars(char *str, int step)
+{
+	int len = strlen(str);
+	char dest[MAX_LEN];
+	memset(dest, 0, len + 1);
+	printf("len = %d", len);
+
+	//strcpy(dest, str + len - step);
+	//strcpy(dest + step, str);
+
+	memcpy(dest, str + len - step, step);
+	memcpy(dest + step, str, len-step);
+	
+	memset(dest + len, 0, 1);
+
+	printf("res = %s", dest);
+}
---
+++ forC/forC/forCPP.h
@@ -0,0 +1,27 @@
+﻿//using namespace std
+
+#include "stdafx.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class withCC
+{
+public:
+	withCC() {}
+	withCC(const withCC&)
+	{
+    	cout << "withCC(withCC&)" << endl;
+	}
+
+	//virtual void showCallOrder();
+	void showCallOrder();
+};
+
+class subCC:public withCC
+{
+public:
+	//virtual void showCallOrder();
+	void showCallOrder();
+};
