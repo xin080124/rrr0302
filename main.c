1f25fb7ac7a833dd0da0bab4f5748d7fed7e76bc
Nancy <nancy@nancy-VirtualBox.(none)>
2017-09-04 07:20:38 +1200

    add test functions

--- main.c
+++ main.c
@@ -2,25 +2,27 @@
 #include<string.h> 
 
 
+
+int testCharPointer();
+
 int main(void)
 {
     //printf("hello, ubuntu again~!");
 	
-	char str[10];
+    char str[10];
+    //char str[1];
 
-	int x = 0; 
+    int x = 0; 
     int y = 0;
 	
-	//char str[1];
-
-	x = strlen(str);
+    x = strlen(str);
     y = sizeof(str);
 	
     int res = sum(10,15);
-	printf("\n the return value is %d",res);
+    printf("\n the return value is %d",res);
 	
 
-       char *pc = (char *)malloc(10);
+    char *pc = (char *)malloc(10);
        //assert(pc);
 
        printf("\n the pc value is %h",(int)pc);
@@ -29,7 +31,14 @@ int main(void)
 	
        printf("\n the pc size is %d \n",z);
 	
-       return 0;
+    testCharPointer();
+    
+    return 0;
+}
+
+int testSize()
+{
+    return 0;
 }
 
 
@@ -38,4 +47,22 @@ int sum(int a, int b)
 	int total=0;
 	total = a + b;
 	return total;
+}
+
+int testCharPointer()
+{
+    //use *str7,then *str7 = "z" will cause crash
+    //but use str7[], it is ok    
+    //char *str7 = "abc";
+    char str7[] = "abc";
+    //char *str8 = "abc";    
+ 
+    printf("\n str7 = 0x%x \n",(int)str7);   
+    //printf("str8 = 0x%x \n",(int)str8);   
+
+    *str7 = "z"; 
+
+    //printf("str8 = %s \n",str8);   
+
+    return 0;
 }
