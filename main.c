e68bc8f02a3e9542ee2d094635729d3edac3b09f
Nancy <nancy@nancy-VirtualBox.(none)>
2017-09-03 16:33:04 +1200

    try -a again

--- main.c
+++ main.c
@@ -6,20 +6,30 @@ int main(void)
 {
     //printf("hello, ubuntu again~!");
 	
-	//char str[10];
+	char str[10];
 
 	int x = 0; 
     int y = 0;
 	
-	char str[1];
+	//char str[1];
 
 	x = strlen(str);
     y = sizeof(str);
 	
     int res = sum(10,15);
-	printf(" the return value is %d",res);
+	printf("\n the return value is %d",res);
 	
-	return 0;
+
+       char *pc = (char *)malloc(10);
+       //assert(pc);
+
+       printf("\n the pc value is %h",(int)pc);
+	
+       int z = sizeof(pc);
+	
+       printf("\n the pc size is %d \n",z);
+	
+       return 0;
 }
 
 
