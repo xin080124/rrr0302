96dd63125bcf72da1457a5db0221610c569a0e95
Nancy <nancy@nancy-VirtualBox.(none)>
2017-09-29 12:22:57 +1300

    binary semophore

--- main.c
+++ main.c
@@ -10,8 +10,9 @@ int testCharPointer();
 int testHeapAddress();
 int testUserInput();
 
-
 int testThread();
+int sum(int a, int b);
+
 
 int main(void)
 {
@@ -29,11 +30,13 @@ int main(void)
     int res = sum(10,15);
     printf("\n the return value is %d",res);
 	
-    testHeapAddress();
+    //testHeapAddress();
 	
-    testCharPointer();
+    //testCharPointer();
     
-    testUserInput();
+    //testUserInput();
+
+    testThread();
     return 0;
 }
 
@@ -54,13 +57,13 @@ int testCharPointer()
 {
     //use *str7,then *str7 = "z" will cause crash
     //but use str7[], it is ok    
-    //char *str7 = "abc";
-    char str7[] = "abc";
+    char *str7 = "abc";
+    //char str7[] = "abc";
     //char *str8 = "abc";    
     printf("\n str7 = 0x%x \n",(int)str7);   
     //printf("str8 = 0x%x \n",(int)str8);   
 
-    *str7 = "z"; 
+    //*str7 = "z"; 
 
     //printf("str8 = %s \n",str8);   
 
@@ -93,11 +96,13 @@ sem_t binSem;
 void* helloWorld(void* arg);
 
 void* helloWorld(void* arg) {
-while(1) {
- // Wait semaphore
-sem_wait(&binSem);
-printf("Hello World/n");
-}
+    while(1) {
+        printf("Hello World wait semaphore,value is %d \n",binSem);    
+
+        // Wait semaphore
+            sem_wait(&binSem);
+    printf("Hello World get semaphore,value is %d \n",binSem);
+    }
 }
 
 int testThread()
@@ -105,36 +110,48 @@ int testThread()
     // Result for System call
     int res = 0;
 
-   // Initialize semaphore
-   res = sem_init(&binSem, 0, 0);
-if (res) {
- printf("Semaphore initialization failed!!/n");
- exit(EXIT_FAILURE);
-}
+    // Initialize semaphore
+    res = sem_init(&binSem, 0, 0);
+    if (res) {
+    printf("Semaphore initialization failed!!/n");
+    exit(EXIT_FAILURE);
+    }
+    printf("Semaphore initialization success!! value is %d \n",binSem);
+    
+    // Create thread
+    pthread_t thdHelloWorld;
+    res = pthread_create(&thdHelloWorld, NULL, helloWorld, NULL);
+    if (res) {
+        printf("Thread creation failed!!/n");
+        exit(EXIT_FAILURE);
+    }
 
- // Create thread
- pthread_t thdHelloWorld;
- res = pthread_create(&thdHelloWorld, NULL, helloWorld, NULL);
-if (res) {
- printf("Thread creation failed!!/n");
- exit(EXIT_FAILURE);
- }
+    //char input[10];
+    //char* input;
+    int input;
 
-while(1) {
- // Post semaphore
- sem_post(&binSem);
- printf("In main, sleep several seconds./n");
-sleep(1);
-}
+        
 
- // Wait for thread synchronization
- void *threadResult;
- res = pthread_join(thdHelloWorld, &threadResult);
-if (res) {
- printf("Thread join failed!!/n");
- exit(EXIT_FAILURE);
-}
+    while(1) {
+        // Post semaphore
+        sem_post(&binSem);
+    
+        printf("In main, sleep several seconds. sem is %d \n",binSem);
+        //sleep(1);
+        scanf("%d",&input);
+        printf("The user has input %d.\n",input);
+    
+    
+    }
 
- exit(EXIT_SUCCESS);
+    // Wait for thread synchronization
+    void *threadResult;
+    res = pthread_join(thdHelloWorld, &threadResult);
+    if (res) {
+        printf("Thread join failed!!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    exit(EXIT_SUCCESS);
 	
 }
