95f09ee5bd9259a1533b16e34ba3ea7b429071d5
YixinZhang <hxy820718@163.com>
2017-09-29 11:15:32 +1300

    remove garbage tokens

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 #include<string.h> 
 
-
+#include <stdio.h> // printf(),
+#include <stdlib.h> // exit(), EXIT_SUCCESS
+#include <pthread.h> // pthread_create(), pthread_join()
+#include <semaphore.h> // sem_init()
 
 int testCharPointer();
 int testHeapAddress();
 int testUserInput();
+
+
+int testThread();
 
 int main(void)
 {
@@ -80,4 +86,55 @@ int testUserInput()
     printf("\n the user input is %d",input);
     //fgets();
     return 0;
+}
+
+sem_t binSem;
+
+void* helloWorld(void* arg);
+
+void* helloWorld(void* arg) {
+while(1) {
+ // Wait semaphore
+sem_wait(&binSem);
+printf("Hello World/n");
+}
+}
+
+int testThread()
+{
+    // Result for System call
+    int res = 0;
+
+   // Initialize semaphore
+   res = sem_init(&binSem, 0, 0);
+if (res) {
+ printf("Semaphore initialization failed!!/n");
+ exit(EXIT_FAILURE);
+}
+
+ // Create thread
+ pthread_t thdHelloWorld;
+ res = pthread_create(&thdHelloWorld, NULL, helloWorld, NULL);
+if (res) {
+ printf("Thread creation failed!!/n");
+ exit(EXIT_FAILURE);
+ }
+
+while(1) {
+ // Post semaphore
+ sem_post(&binSem);
+ printf("In main, sleep several seconds./n");
+sleep(1);
+}
+
+ // Wait for thread synchronization
+ void *threadResult;
+ res = pthread_join(thdHelloWorld, &threadResult);
+if (res) {
+ printf("Thread join failed!!/n");
+ exit(EXIT_FAILURE);
+}
+
+ exit(EXIT_SUCCESS);
+	
 }
