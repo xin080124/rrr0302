16aab00e998103778e604ece3318244cd7a69181
Nancy <nancy@nancy-VirtualBox.(none)>
2017-10-05 09:50:33 +1300

    see the index value in each iteration

--- forUbt/yxAlgorithm.c
+++ forUbt/yxAlgorithm.c
@@ -3,8 +3,6 @@
 
 static int BSearchR(int a[],int x,int low,int high);
 static int BSearch(int a[],int key,int n);
-static int interpolationSearch(int arr[], int n, int x);
-
 
 int testSearch(yxSearch pfunc)
 {
@@ -30,45 +28,10 @@ int binarySearchNormal(int targetNum,int *arr)
     return res;
 }
 
-int insertSearch(int targetNum,int n,int *arr)
-{
-	int res = 0;
-	res = interpolationSearch(arr, n, targetNum);
-    return res;
-}
-
-static int interpolationSearch(int arr[], int n, int x)
-{
-	    // Find indexes of two corners
-    int lo = 0, hi = (n - 1);
- 
-    // Since array is sorted, an element present
-    // in array must be in range defined by corner
-    while (lo <= hi && x >= arr[lo] && x <= arr[hi])
-    {
-        // Probing the position with keeping
-        // uniform distribution in mind.
-        int pos = lo + (((double)(hi-lo) /
-              (arr[hi]-arr[lo]))*(x - arr[lo]));
- 
-        // Condition of target found
-        if (arr[pos] == x)
-            return pos;
- 
-        // If x is larger, x is in upper part
-        if (arr[pos] < x)
-            lo = pos + 1;
- 
-        // If x is smaller, x is in lower part
-        else
-            hi = pos - 1;
-    }
-    return -1;
-
-}
-
 static int BSearchR(int a[],int x,int low,int high)
 {
+    printf("\n BSearchR low = %d high = %d \n",low,high);
+
 	int mid;
 	if(low>high) return -1;
 	mid=(low+high)/2;
@@ -87,6 +50,7 @@ static int BSearch(int a[],int key,int n)
 		if(a[mid]==key) return mid;
 		else if(a[mid]<key) low=mid+1;
 		else high=mid-1;
+            printf("\n BSearch low = %d high = %d \n",low,high);
 	}
 	return -1;
 }
