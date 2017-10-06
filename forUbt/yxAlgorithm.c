da91dc5797f516791db6654eec5dd8b223c5607d
YixinZhang <hxy820718@163.com>
2017-10-07 07:19:15 +1300

    add sample code of interpolation search

--- forUbt/yxAlgorithm.c
+++ forUbt/yxAlgorithm.c
@@ -3,6 +3,8 @@
 
 static int BSearchR(int a[],int x,int low,int high);
 static int BSearch(int a[],int key,int n);
+static int interpolationSearch(int arr[], int n, int x);
+
 
 int testSearch(yxSearch pfunc)
 {
@@ -26,6 +28,43 @@ int binarySearchNormal(int targetNum,int *arr)
 	int res = 0;
 	res = BSearch(arr,targetNum,6);
     return res;
+}
+
+int insertSearch(int targetNum,int n,int *arr)
+{
+	int res = 0;
+	res = interpolationSearch(arr, n, targetNum);
+    return res;
+}
+
+static int interpolationSearch(int arr[], int n, int x)
+{
+	    // Find indexes of two corners
+    int lo = 0, hi = (n - 1);
+ 
+    // Since array is sorted, an element present
+    // in array must be in range defined by corner
+    while (lo <= hi && x >= arr[lo] && x <= arr[hi])
+    {
+        // Probing the position with keeping
+        // uniform distribution in mind.
+        int pos = lo + (((double)(hi-lo) /
+              (arr[hi]-arr[lo]))*(x - arr[lo]));
+ 
+        // Condition of target found
+        if (arr[pos] == x)
+            return pos;
+ 
+        // If x is larger, x is in upper part
+        if (arr[pos] < x)
+            lo = pos + 1;
+ 
+        // If x is smaller, x is in lower part
+        else
+            hi = pos - 1;
+    }
+    return -1;
+
 }
 
 static int BSearchR(int a[],int x,int low,int high)
