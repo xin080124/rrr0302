464cf9dd8ef02c30425fc40ee2132ef7dbf973e1
nancyUnitec <xin080124@hotmail.com>
2017-12-04 22:28:50 +1300

    add steps

--- sqlForLR.sql
+++ sqlForLR.sql
@@ -1,6 +1,8 @@
 step1: gather all of the data:
 show_001InitialAssocTable  
+
 From sales and claims
+
 SELECT s.PolicyId as PolicyId, YrPurchased as YrSales, mthPurchased as MthSales, s.premium_exGst as Premium_exGst, c.ClaimId as ClaimId, 
   s.productClass as Class, s.productBrand as Brand, s.productModel as Model, c.claimCostexGST as ClaimCost,
   ((YEAR(ClaimDate) - YrPurchased)*12 + (MONTH(ClaimDate) - mthPurchased) ) AS MthClaim,
@@ -11,3 +13,19 @@ FROM (SELECT PolicyId, SalesWeek, premium_exGst, productClass, productBrand, pro
 LEFT OUTER JOIN [localcover-55:ling.claims] AS c
 ON s.PolicyId = c.PolicyId
 ORDER BY SalesWeek
+
+
+
+step2: generate expected data
+002expectedDataFrom2015
+
+from 001InitialAssocTable
+
+SELECT YrSales, Sales, Claims, Claims / Sales as FailureRate, Premium, ClaimCost, ClaimCost / Claims as Severity, 
+FROM (SELECT YrSales, COUNT(DISTINCT PolicyId) as Sales, COUNT(DISTINCT ClaimId) AS Claims, 
+    SUM(Premium_exGst) as Premium,
+    SUM(ClaimCost) as ClaimCost
+  FROM [localcover-55:lc_api_show.001InitialAssocTable]
+  WHERE YrSales = 2015
+  GROUP BY YrSales
+  ORDER BY YrSales) as t
